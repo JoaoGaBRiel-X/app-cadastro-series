2ebfd5f7c95108ecd038018f73805afc5de5b974
JoaoGaBRiel-X <joaogabriel.mc@hotmail.com>
2022-01-28 11:06:33 -0300

    Commit inicial. Sem modificações

---
+++ Entities/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace app_cadastro_series.Entities
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Entities/SerieRepositorio.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using app_cadastro_series.Interfaces;
+
+namespace app_cadastro_series.Entities
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Exclui();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
